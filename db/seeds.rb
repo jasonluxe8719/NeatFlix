2cf6d0bee3f95f96e9922786ed8771546e6329ce
Jason <jasonluxe8719@gmail.com>
2020-06-24 20:00:18 -0500

    Seeded Demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+demouser = User.create({email: "demouser@neatflix.com", password: "123456"})
